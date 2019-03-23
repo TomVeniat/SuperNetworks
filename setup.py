4a00dc5ae605507ee43796589445a725a4568760
Tom Veniat <tom.veniat@lip6.fr>
2019-03-23 16:46:04 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='supernets',
-      version='0.0.3',
+      version='0.0.4',
       description='A module containing the basics to use Super Networks',
       long_description=long_description,
       long_description_content_type="text/markdown",
